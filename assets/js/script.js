1ed24be8b2888b5ea1b9e9277faa8909da8c0fd2
StanislasE <stanislas.etienne@gmail.com>
2020-11-09 12:02:07 +0100

    fait

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$('#red').click(function(){
+    $('#text').css('color', '#CD5C5C');
+});
+$('#blue').click(function(){
+    $('#text').css('color', '#0000FF');
+});
+$('#green').click(function(){
+    $('#text').css('color', '#008000');
+});
